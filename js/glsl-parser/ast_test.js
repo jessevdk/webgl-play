10a1ea2e8f171d878be33d4d80dc258b139a3f84
Jesse van den Kieboom <jessevdk@gmail.com>
2014-08-13 23:16:48 +0200

    Factor out for error tests

--- js/glsl-parser/ast_test.js
+++ js/glsl-parser/ast_test.js
@@ -6,43 +6,55 @@ var assert = require('chai').assert;
 
 var files = fs.readdirSync('testfiles');
 
-suite('ast', function() {
+function scan_files(filt) {
+    var ret = [];
+
     for (var i = 0; i < files.length; i++) {
         var f = files[i];
+        var ff = 'testfiles/' + f;
 
-        if (f.indexOf('ast_') != 0 ||
-            f.lastIndexOf('.glslv') != f.length - 6) {
-            continue;
+        if (filt(files[i]) && f.lastIndexOf('.glslv') == f.length - 6 && fs.existsSync(ff + '.ast')) {
+            ret.push(f);
         }
-
-        if (!fs.existsSync('testfiles/' + f + '.ast')) {
-            continue;
-        }
-
-        test(f.slice(4, f.length - 6), (function(f) {
-            var unprocessed = fs.readFileSync('testfiles/' + f, 'utf8');
-            var p = new glsl.ast.Parser(unprocessed);
-
-            var errors = p.errors();
-
-            for (var e = 0; e < errors.length; e++) {
-                assert.ok(false, errors[e].formatted_message() + '\n' + errors[e]._stack);
-            }
-
-            var astj = fs.readFileSync('testfiles/' + f + '.ast', 'utf-8');
-
-            if (!astj) {
-                astj = '[]';
-            }
-
-            var seen = {};
-
-            var ast = JSON.parse(astj);
-            var body = p.marshal();
-
-            assert.deepEqual(body, ast);
-        }).bind(this, f));
     }
+
+    return ret;
+}
+
+function make_suite(name, prefix, filter) {
+    var files = scan_files(filter);
+
+    suite(name, function() {
+        for (var i = 0; i < files.length; i++) {
+            var f = files[i];
+
+            test(f.slice(prefix.length, f.length - 6), (function(f) {
+                var unprocessed = fs.readFileSync('testfiles/' + f, 'utf8');
+                var p = new glsl.ast.Parser(unprocessed);
+
+                var astj = fs.readFileSync('testfiles/' + f + '.ast', 'utf-8');
+
+                if (!astj) {
+                    astj = '{}';
+                }
+
+                var seen = {};
+
+                var ast = JSON.parse(astj);
+                var body = p.marshal();
+
+                assert.deepEqual(body, ast);
+            }).bind(this, f));
+        }
+    });
+}
+
+make_suite('ast', 'ast_', function(f) {
+    return f.indexOf('ast_') == 0 && f.indexOf('ast_error_') != 0;
+});
+
+make_suite('ast-error', 'ast_error_', function(f) {
+    return f.indexOf('ast_error_') == 0;
 });
 
 // vi:ts=4:et
