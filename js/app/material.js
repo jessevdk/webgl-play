e76ca4a42e59b96cf0dd01a5cf4e440021caa382
Jesse van den Kieboom <jessevdk@gmail.com>
2014-10-12 11:14:04 +0200

    Add documentation for material

--- js/app/material.js
+++ js/app/material.js
@@ -1,11 +1,38 @@
 var math = require('./math');
 
+/**
+ * A basic material. A material contains a specification of the material
+ * properties used to render a model. These material properties are transfered
+ * to a glsl program as uniforms.
+ *
+ * @param ctx the context.
+ * @constructor
+ */
 function Material(ctx) {
+    /**
+     * A map of uniform names to values. Any values set in this map,
+     * with a corresponding uniform in the material program, are
+     * automatically bound when the material is used. Use the
+     * various math.vec/mat types to map to the glsl types.
+     */
     this.uniforms = {
         color: math.vec4.fromValues(1, 0, 1, 0)
     };
 
+    /**
+     * Sets the visibility of the material. The value should be one of
+     * Material.VISIBLE, Material.INVISIBLE or Material.ONLY_CHILDREN.
+     * Models with an invisible material are not rendered at all, while
+     * models with a material visibility set to Material.ONLY_CHILDREN will
+     * render only the models children (but not the model itself) (defaults
+     * to Material.VISIBLE).
+     */
     this.visible = true;
+
+    /**
+     * The name of the program to render, or null to use the default
+     * program (defaults to null).
+     */
     this.program = null;
 }
 
@@ -80,6 +107,18 @@ Material.prototype._setUniforms = function(ctx, p, uniforms) {
     }
 }
 
+/**
+ * Bind the material in the given context. This sets up the material
+ * program as well as binding all the material uniforms. Additional
+ * non-material uniforms can be provided (for example the modelViewProjection
+ * matrix).
+ *
+ * Note that this is automatically called when rendering a model and
+ * should usually not be called by the user.
+ *
+ * @param ctx the context.
+ * @param uniforms a map of additional uniform values to set.
+ */
 Material.prototype.bind = function(ctx, uniforms) {
     var p = ctx.findProgram(this.program);
 
@@ -96,8 +135,13 @@ Material.prototype.bind = function(ctx, uniforms) {
     this._setUniforms(ctx, p, uniforms);
 }
 
-Material.INVISIBLE = false;
+/** Visible, renders the model normally. */
 Material.VISIBLE = true;
+
+/** Invisible, will not render the model. */
+Material.INVISIBLE = false;
+
+/** Only children, renders only the children, but not the model itself. */
 Material.ONLY_CHILDREN = 2;
 
 module.exports = Material;
