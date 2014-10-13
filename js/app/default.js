c309891c7589c404e97d97bb45b8f2ad2e15b4a1
Jesse van den Kieboom <jessevdk@gmail.com>
2014-10-13 14:59:59 +0200

    Update default javascript program

--- js/app/default.js
+++ js/app/default.js
@@ -1,7 +1,11 @@
 function init(c) {
     this.box = new c.models.Box(c, 1, 1, 1);
-    this.view = c.models.View.perspective(c, 60, null, 0.01, 50);
-    this.view.transform.$translateZ(5);
+    this.view = c.models.View.perspective(c, 45, null, 0.01, 50);
+
+    this.view.transform
+        .rotateY(-Math.PI / 6)
+        .rotateX(-Math.PI / 6)
+        .translateForward(3);
 }
 
 function render(c) {
