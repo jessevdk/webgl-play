1dff22dca660b9291f0080b34253e15433216c8d
Jesse van den Kieboom <jessevdk@gmail.com>
2014-11-19 14:25:15 +0100

    Added donate button in about dialog

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -42,6 +42,23 @@ var Settings = {
         dataQuery: function(hash) {
             return 'http://jessevdk.github.io/webgl-play/?d=' + hash;
         }
+    },
+
+    hooks: {
+        about: function(ul) {
+            var donate = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"> \
+                <input type="hidden" name="cmd" value="_s-xclick"> \
+                <input type="hidden" name="hosted_button_id" value="MM6LF8RSX2UWC"> \
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"> \
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"> \
+            </form>';
+
+            var li = document.createElement('li');
+            li.classList.add('donate');
+
+            li.innerHTML = donate;
+            ul.appendChild(li);
+        }
     }
 };
 
