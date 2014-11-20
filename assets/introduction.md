5094d19e1fd48eb2733aa8cce23bb325ee6886c5
Jesse van den Kieboom <jessevdk@gmail.com>
2014-11-20 11:36:02 +0100

    Added introduction

---
+++ assets/introduction.md
@@ -0,0 +1,18 @@
+## WebGL Playground Introduction
+
+Hi, and welcome! The WebGL Playground is a web-based, live editing environment primarily created to allow easy experimenting with WebGL.
+The main editor consists of a vertex shader editor, fragment shader editor, JavaScript editor and a live view of the current rendering.
+Rendering updates automatically as you type, while reporting any errors inline in the editors. A simple UI system allows you to create
+interactive demos.
+
+This website is very much still in beta, which means that you *will* encounter certain problems. If you do, please consider being a good
+open source citizen and open an issue for it on the [issue tracker](https://github.com/jessevdk/webgl-play/issues). Most notable issues
+at the moment are:
+
+1. Limited amount of testing across browsers and platforms. The playground has been for the moment mostly tested on a single Mac using Chrome and to a lesser extend Firefox. It has not been tested *at all* at the moment on Internet Explorer and problems are expected to occur for untested combinations of platforms and browsers. Please file issues for any problems you encounter.
+2. There is no available documentation at the moment, neither for how to use the website, nor for the available API when writing the JavaScript code for your experiments. Please refer to the examples in the gallery for the moment while the documentation is being worked on.
+
+*Although I generally do not do this, I ask you kindly that if you find this website personally useful and valuable, to consider making a
+(very) small donation to help sustain it. There is a donate button in the **About** menu. Thanks!*
+
+[Source on github](https://github.com/jessevdk/webgl-play)
--- assets/js/settings.js
+++ assets/js/settings.js
@@ -58,6 +58,10 @@ var Settings = {
 
             li.innerHTML = donate;
             ul.appendChild(li);
+        },
+
+        intro: function() {
+            return "## WebGL Playground Introduction\n\nHi, and welcome! The WebGL Playground is a web-based, live editing environment primarily created to allow easy experimenting with WebGL.\nThe main editor consists of a vertex shader editor, fragment shader editor, JavaScript editor and a live view of the current rendering.\nRendering updates automatically as you type, while reporting any errors inline in the editors. A simple UI system allows you to create\ninteractive demos.\n\nThis website is very much still in beta, which means that you *will* encounter certain problems. If you do, please consider being a good\nopen source citizen and open an issue for it on the [issue tracker](https://github.com/jessevdk/webgl-play/issues). Most notable issues\nat the moment are:\n\n1. Limited amount of testing across browsers and platforms. The playground has been for the moment mostly tested on a single Mac using Chrome and to a lesser extend Firefox. It has not been tested *at all* at the moment on Internet Explorer and problems are expected to occur for untested combinations of platforms and browsers. Please file issues for any problems you encounter.\n2. There is no available documentation at the moment, neither for how to use the website, nor for the available API when writing the JavaScript code for your experiments. Please refer to the examples in the gallery for the moment while the documentation is being worked on.\n\n*Although I generally do not do this, I ask you kindly that if you find this website personally useful and valuable, to consider making a\n(very) small donation to help sustain it. There is a donate button in the **About** menu. Thanks!*\n\n[Source on github](https://github.com/jessevdk/webgl-play)\n";
         }
     }
 };
